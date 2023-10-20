07caa51a8caa8832a8fd6252b317b6a598de6235
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-20 17:41:23 +0100

    Create functions to validate password on change.

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ form.addEventListener('submit', (event) => confirmPwd(event));
 function confirmPwd(event) {
     const pwd = document.querySelector('#password');
     const pwdConfirmed = document.querySelector('#confirm_password');
-    if(pwd.value !== pwdConfirmed.value) {
+    if(pwd.value !== pwdConfirmed.value || pwd === '') {
         event.preventDefault();
-        return alert('no match');
+        alert('*Passwords do not match')
+    }
+};
+
+const pwdInput = document.querySelector('#password')
+const pwdConfirmInput = document.querySelector('#confirm_password');
+const errorMsg = document.querySelector('div.error')
+const errorElements = document.querySelectorAll('.error');
+
+pwdInput.addEventListener('change', () => checkPwd());
+pwdConfirmInput.addEventListener('change', () => checkPwd());
+
+function checkPwd () {
+    const pwd = document.querySelector('#password');
+    const pwdConfirmed = document.querySelector('#confirm_password');
+    if(pwd.value !== pwdConfirmed.value) {
+        errorElements.forEach((element) => element.classList.add('error'));
+        errorMsg.textContent = '*Passwords do not match';
     } else {
-        return alert('match');
+        errorElements.forEach((element) => element.classList.remove('error'));
+        errorMsg.textContent = '';
     }
 };
