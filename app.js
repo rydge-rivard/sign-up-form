dc7dd8374df65ac281067673bcc3b5e78141b09e
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-20 15:34:06 +0100

    Add js password matching function on submit event.

---
+++ app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => confirmPwd(event));
+
+function confirmPwd(event) {
+    const pwd = document.querySelector('#password');
+    const pwdConfirmed = document.querySelector('#confirm_password');
+    if(pwd.value !== pwdConfirmed.value) {
+        event.preventDefault();
+        return alert('no match')
+    } else {
+        return alert('match')
+    }
+}
